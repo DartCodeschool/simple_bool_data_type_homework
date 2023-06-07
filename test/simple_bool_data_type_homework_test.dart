e27c09d72e2c6280372f8051bc3478ed6366322f
Sanjar17 <sanjarsaidov173@gmail.com>
2023-06-07 15:10:54 +0500

    unnexary

--- test/simple_bool_data_type_homework_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:simple_bool_data_type_homework/simple_bool_data_type_homework.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
